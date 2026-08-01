138caaef6dfbacbd51d260a60e12ac97b47d93be
rajyadav38 <rajyadav5566788@gmail.com>
2026-08-01 13:24:38 +0530

    Implemented and learned the preorder, postorder , inorder and levelOrder traversal for BT and Did count of Node question

--- BinaryTree/BT.java
+++ BinaryTree/BT.java
@@ -1,5 +1,5 @@
 package BinaryTree;
-
+import java.util.*;
 public class BT {
     static class Node{
         int data;
@@ -35,12 +35,68 @@ public class BT {
         preorder(root.left);
         preorder(root.right);
     }
+
+    public static void inorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void levelOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()) {
+            Node currNode = q.remove();
+            if(currNode == null) {
+                System.out.println();
+                if(q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if(currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            } 
+            }
+        }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
+        System.out.print("Preorder: ");
         preorder(root);
-
+        System.out.println();
+        System.out.print("Inorder: ");
+        inorder(root);
+        System.out.println();
+        System.out.print("Postorder: ");
+        postorder(root);
+        System.out.println();
+        System.out.println("LevelOrder: ");
+        levelOrder(root);
     }
 }
