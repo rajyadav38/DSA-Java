617557f1346027be7f5177d2887a88fb06e4b674
rajyadav38 <rajyadav5566788@gmail.com>
2026-08-16 00:13:59 +0530

    implemented and understanded all the functions Code of HashMap

--- Hashmap/hashMapPut.java
+++ Hashmap/hashMapPut.java
@@ -16,7 +16,7 @@ public class hashMapPut {
     private int N; // N = buckets
     private LinkedList<Node> buckets[]; // N = buckets.length
 
-    @SuppressWarnings("unchecked");
+    @SuppressWarnings("unchecked")
     public HashMap() {
         this.N = 4;
         this.buckets = new LinkedList[4];
@@ -24,7 +24,129 @@ public class hashMapPut {
             this.buckets[i] = new LinkedList<>();
         }
     }
+    private int hashFunction(K key) {
+        int bi = key.hashCode();
+        return Math.abs(bi) % N;
+    }
 
+    private int searchInLL(K key , int bi) {
+        LinkedList<Node> ll = buckets[bi];
+        for(int i=0; i<ll.size(); i++) {
+            if(ll.get(i).key == key) {
+                return i; //di
+            }
+         }
 
+         return -1;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void reshash() {
+        LinkedList<Node> oldBucket[] = buckets;
+        buckets = new LinkedList[N*2];
+
+        for(int i = 0; i<N*2; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+
+        for(int i = 0; i<oldBucket.length; i++) {
+            LinkedList<Node> ll = oldBucket[i];
+            for(int j = 0; j<ll.size(); j++) {
+                Node node = ll.get(j);
+                put(node.key, node.value);
+            }
+        }
+    }
+
+    public void put(K key , V value) {
+        int bi = hashFunction(key);
+        int di = searchInLL(key , bi);
+
+        if(di == -1) { // key don't exist
+            buckets[bi].add(new Node(key, value));
+            n++;
+        } 
+        else { //Key exist
+            Node node = buckets[bi].get(di);
+            node.value = value;
+        }
+
+        double lambda = (double)n/N;
+        if(lambda > 2.0) {
+            //rehashing
+            reshash();
+        }
+    }
+
+    public boolean containsKey(K key) {
+        int bi = hashFunction(key);
+        int di = searchInLL(key , bi);
+
+        if(di == -1) { // key don't exist
+            return false;
+        } 
+        else { //Key exist
+            return true;
+        }
+    }
+
+    public V remove(K key) {
+        int bi = hashFunction(key);
+        int di = searchInLL(key , bi);
+
+        if(di == -1) { // key don't exist
+            return null;
+        } 
+        else { //Key exist
+            Node node = buckets[bi].remove(di);
+            n--;
+            return node.value;
+        }
+    }
+
+    public V get(K key) {
+        int bi = hashFunction(key);
+        int di = searchInLL(key , bi);
+
+        if(di == -1) { // key don't exist
+            return null;
+        } 
+        else { //Key exist
+            Node node = buckets[bi].get(di);
+            return node.value;
+        }
+    }
+
+    public ArrayList<K> keySet() {
+        ArrayList<K> keys = new ArrayList<>();
+
+        for(int i = 0; i<buckets.length; i++) {
+            LinkedList<Node> ll = buckets[i];
+            for(int j = 0; j<ll.size(); j++){
+                Node node = ll.get(j);
+                keys.add(node.key);
+            }
+        }
+        return keys;
+    }
+
+    public  boolean isEmpty() {
+        return n == 0;
+    }
+    }
+    public static void main(String[] args) {
+        HashMap<String , Integer> map = new HashMap<>();
+        map.put("India", 190);
+        map.put("China", 200);
+        map.put("US", 50);
+        map.put("Japan", 40);
+
+        ArrayList<String> keys = map.keySet();
+        for(int i = 0; i<keys.size(); i++) {
+            System.out.println(keys.get(i)+" "+map.get(keys.get(i)));
+        }
+
+        map.remove("India");
+        System.out.println(map.get("India"));
     }
 }
