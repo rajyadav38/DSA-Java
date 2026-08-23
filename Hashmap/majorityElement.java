d3b32ec36e6f63b694113cfff1b435fa24af5a49
rajyadav38 <rajyadav5566788@gmail.com>
2026-08-23 23:36:30 +0530

    solved one question of Hashmap called Majority element

---
+++ Hashmap/majorityElement.java
@@ -0,0 +1,26 @@
+package Hashmap;
+import java.util.*;
+public class majorityElement {
+
+    public static void majority(int nums[]) {
+        HashMap<Integer , Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i<n; i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1); 
+            }
+        }
+
+        for(int key : map.keySet()) {
+            if(map.get(key) > n/3) {
+                System.out.println(key);
+            }
+        }
+    }
+    public static void main(String[] args) { // O(n)
+        int nums[] = {1,2};
+        majority(nums);
+    }
+}
